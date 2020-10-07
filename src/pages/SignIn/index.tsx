11a76037929f41cfc2ee81ac70a2b62d94325e2c
hugo miranda <hugo.miranda.souza@gmail.com>
2020-10-06 21:30:31 -0300

    Initial signIn page layout

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 
 import { Image } from 'react-native';
-import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu Logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button
+        onPress={() => {
+          console.log('uhu');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
